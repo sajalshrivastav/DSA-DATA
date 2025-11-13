87b86f0a36fefa2f609d3ec2e477268cbe56d150
Sajal Shrivastav <93852168+sajalshrivastav@users.noreply.github.com>
2025-11-13 22:02:26 +0530

    Add Longest Consecutive Sequence (NeetCode)

--- DSA/NeetCode/JavaScript/longest_consecutive_sequence.js
+++ DSA/NeetCode/JavaScript/longest_consecutive_sequence.js
@@ -1,19 +1,19 @@
-class Solution {class Solution {
-    /**    /**
-     * @param {number[]} nums     * @param {number[]} nums
-     * @return {number}     * @return {number}
-     */     */
-    longestConsecutive(nums) {    longestConsecutive(nums) {
-        let maxLength = 0;        let maxLength = 0;
-        for(let i = 0; i < nums.length; i++){        for(let i = 0; i < nums.length; i++){
-          let currentNum = nums[i];          let currentNum = nums[i];
-          let currentLength = 1;          let currentLength = 1;
-          while(nums.includes(currentNum + 1)){          while(nums.includes(currentNum + 1)){
-            currentNum += 1;            currentNum += 1;
-            currentLength += 1;            currentLength += 1;
-          }           } 
-          maxLength = Math.max(maxLength , currentLength);          maxLength = Math.max(maxLength , currentLength);
-        }        }
-        return maxLength        return maxLength
-    }    }
-}}
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let maxLength = 0;
+        for(let i = 0; i < nums.length; i++){
+          let currentNum = nums[i];
+          let currentLength = 1;
+          while(nums.includes(currentNum + 1)){
+            currentNum += 1;
+            currentLength += 1;
+          } 
+          maxLength = Math.max(maxLength , currentLength);
+        }
+        return maxLength
+    }
+}
